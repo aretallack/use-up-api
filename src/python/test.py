43f79c9db00482638b9761ab7f2d2228471cb811
@aretallack <aeretallack17@gmail.com>
2024-06-02 21:34:04 +0930

    finished inital tests

--- src/python/test.py
+++ src/python/test.py
@@ -15,14 +15,35 @@ api_key = os.getenv('API_KEY')
 ##### VERIFY API #####
 ######################
 
-url = "https://api.up.com.au/api/v1/util/ping"
+base_url = 'https://api.up.com.au/api/v1'
 headers = {
     "Authorization": f'Bearer {api_key}'
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(F'{base_url}/util/ping', headers=headers)
 
 if response.status_code == 200:
     print("Success:", response.json())
 else:
     print(f"Error {response.status_code}: {response.text}")
+
+#%%########
+##### #####
+###########
+
+# Accounts
+
+params = {
+   'page[size]': 2,
+   'filter[accountType]': 'SAVER'
+}
+
+accounts = requests.get(f'{base_url}/accounts', headers = headers, params = params)
+# accounts = requests.get(f'{base_url}/accounts', headers = headers)
+accounts.json()
+
+#%%#####################
+##### TRANSACTIONS #####
+########################
+transactions = requests.get(f'{base_url}/transactions', headers = headers)
+transactions.json()
